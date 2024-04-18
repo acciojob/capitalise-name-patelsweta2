a9ef0b91ad191bfca136a277500b60f9581afe5b
patelsweta2 <80040744+patelsweta2@users.noreply.github.com>
2024-04-18 18:48:11 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 //your JS code here. If required.
+
+const input = document.getElementById('fname');
+
+input.addEventListener("blur", function(){
+	let name = input.value;
+	name = name.toUpperCase();
+	input.value = name;
+})
